74d1c2ee063e8d3d720c83e2921379f0328974da
Basadev <basadev4@gmail.com>
2016-05-13 00:47:35 -0400

    parse server example working

--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,12 @@
-// var schedule = require('node-schedule');
-// var moment = require("moment");
+var schedule = require('node-schedule');
+var moment = require("moment");
 
 
-// var rule = new schedule.RecurrenceRule();
-// rule.minute = 1;
-// var j = schedule.scheduleJob(rule, function(){
-//   console.log('The answer to life, the universe, and everything!');
-// });
+var rule = new schedule.RecurrenceRule();
+rule.minute = 1;
+var j = schedule.scheduleJob(rule, function(){
+  console.log('The answer to life, the universe, and everything!');
+});
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi Igsaan. DocWhere is live :D');
